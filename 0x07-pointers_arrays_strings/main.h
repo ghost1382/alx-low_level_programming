eacf67b6d713b896402092fe4ba3d4cf56fa297e
ghost1382 <mohammed.bn884@gmail.com>
2023-07-31 14:50:07 -0700

    update

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _memset - Fills the first n bytes of the memory area pointed to by s
+ *           with the constant byte b.
+ * @s: Pointer to the memory area to be filled.
+ * @b: The constant byte to fill the memory with.
+ * @n: Number of bytes to fill.
+ *
+ * Return: Pointer to the memory area s.
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H */
+
