819e250392cfb0ef44334c72d040b100b1702a63
ghost1382 <mohammed.bn884@gmail.com>
2023-08-10 14:15:24 -0700

    update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,27 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Other necessary includes */
+#include <stdlib.h>
 
+/* Task 0: malloc_checked */
+void *malloc_checked(unsigned int b);
+
+/* Task 1: string_nconcat */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/* Task 2: _calloc */
 void *_calloc(unsigned int nmemb, unsigned int size);
 
-/* Other function prototypes or declarations */
+/* Task 3: array_range */
+int *array_range(int min, int max);
+
+/* Task 4: _realloc */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+/* Task 5: Advanced Multiplication */
+int _putchar(char c);
+void print_number(char *number);
+char *multiply(char *num1, char *num2);
 
 #endif /* MAIN_H */
 
